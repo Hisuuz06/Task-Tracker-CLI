1ca4edd6beb9f12441e2927ee25bf9461a9b48d0
Hisuuz06 <24021474@vnu.edu.vn>
2026-04-04 00:19:05 +0700

    add function

--- task.py
+++ task.py
@@ -7,3 +7,10 @@ class Task:
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
         Task.id += 1
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat(),
+        }
--- taskcli.py
+++ taskcli.py
@@ -1,24 +1,32 @@
 import task
-from task import Task
 import json
 import argparse
 
+from task import Task
+
 file_path = "task.json"
 
-def load_data(file_path):
+def load_data():
     try:
         with open(file_path, "r") as f:
             return json.load(f)
     except FileNotFoundError:
         print("Tasks file not found.")
 
-def save_data(data, file_path):
+def save_data(tasks):
     try:
         with open(file_path, "w") as f:
-            json.dump(data, f)
+            json.dump(tasks, f, indent=4)
     except FileNotFoundError:
         print("Tasks file not found.")
 
-save_data({"hi" : "hello"}, file_path)
-task_data = load_data(file_path)
-print(task_data)
+def add(name_task):
+    task = Task(name_task)
+    tasks = load_data()
+    tasks[task.id] = task.to_dict()
+    save_data(tasks)
+    print(f"Task added. ID: {task.id}")
+
+
+add("Learn Python")
+add("Learn CLI")
